4a605d1611d137fe2d32641189ebb95090c1d335
Erick Roberto Martinez <leonrkmtz@gmail.com>
2017-11-24 04:42:56 +0200

    ejercicio pulpo con java

--- myScript.js
+++ myScript.js
@@ -1,4 +1,11 @@
-alert("Welcome to your Bank")
-var deposit = prompt("How much would you like to deposit today")
-alert("You've deposited: "+ deposit)
-console.log("you're a cool person ");
+var formaText = document.querySelector("h1");
+var formaTextdos = document.querySelector("h3");
+var boton = document.querySelector(".button");
+
+boton.addEventListener("click", function(){
+
+  formaText.setAttribute("class","uno");
+  formaTextdos.setAttribute("class","dos py-3");
+  boton.setAttribute("class","boton");
+
+});
